8adde8c23bffa4b4719b1f94569aec80fd51b936
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-06-05 21:03:14 +0530

    initial commit

---
+++ components/shared/footer.tsx
@@ -0,0 +1,11 @@
+import { ThemeSwitcher } from "../theme-switcher";
+export default function Footer() {
+return(
+    <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+          <p>
+            White Waters
+          </p>
+          <ThemeSwitcher />
+        </footer>
+);
+}
