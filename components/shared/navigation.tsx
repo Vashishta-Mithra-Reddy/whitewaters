09ea7b8d41481b2e4cfbe9187483d14fc6ec21ce
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-08-05 23:47:15 +0530

    nav fix

--- components/shared/navigation.tsx
+++ components/shared/navigation.tsx
@@ -9,21 +9,24 @@ export default function Navigation() {
               {/* <Link href={"/services"} className="text-foreground px-5 py-2 rounded-lg hover:text-foreground hover:bg-foreground/10">
                 Services
               </Link> */}
+              {/* <Link href={"/home"} className={`text-foreground px-5 py-2 rounded-lg hover:text-foreground hover:bg-foreground/10 ${pathname==="/home"?"bg-foreground/10 border-2 border-dashed border-foreground/10":""}`}>
+                Home
+              </Link> */}
               <Link href={"/kayaking"} className={`text-foreground px-5 py-2 rounded-lg hover:text-foreground hover:bg-foreground/10 ${pathname==="/kayaking"?"bg-foreground/10 border-2 border-dashed border-foreground/10":""}`}>
                 Kayaking
               </Link>
               <Link href={"/rafting"} className={`text-foreground px-5 py-2 rounded-lg hover:text-foreground hover:bg-foreground/10 ${pathname==="/rafting"?"bg-foreground/10 border-2 border-dashed border-foreground/10":""}`}>
                 Rafting
               </Link> 
-              <Link href={"/hiking"} className={`text-foreground px-5 py-2 rounded-lg hover:text-foreground hover:bg-foreground/10 ${pathname==="/hiking"?"bg-foreground/10 border-2 border-dashed border-foreground/10":""}`}>
+              {/* <Link href={"/hiking"} className={`text-foreground px-5 py-2 rounded-lg hover:text-foreground hover:bg-foreground/10 ${pathname==="/hiking"?"bg-foreground/10 border-2 border-dashed border-foreground/10":""}`}>
                 Hiking
-              </Link>
+              </Link> */}
               <Link href={"/trekking"} className={`text-foreground px-5 py-2 rounded-lg hover:text-foreground hover:bg-foreground/10 ${pathname==="/trekking"?"bg-foreground/10 border-2 border-dashed border-foreground/10":""}`}>
                 Trekking
               </Link> 
-              <Link href={"/pilgrimage"} className={`text-foreground px-5 py-2 rounded-lg hover:text-foreground hover:bg-foreground/10 ${pathname==="/pilgrimage"?"bg-foreground/10 border-2 border-dashed border-foreground/10":""}`}>
+              {/* <Link href={"/pilgrimage"} className={`text-foreground px-5 py-2 rounded-lg hover:text-foreground hover:bg-foreground/10 ${pathname==="/pilgrimage"?"bg-foreground/10 border-2 border-dashed border-foreground/10":""}`}>
                 Pilgrimage
-              </Link> 
+              </Link>  */}
               {/* <Link href={"/about"} className="text-muted-foreground">
                 About
               </Link>
